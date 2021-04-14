0b679654a8cde244e43d8c2c65681250e503be58
sreeragnair82 <sreeragnair82@gmail.com>
2021-04-15 01:45:40 +0530

    3rd commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
+import React, { useState } from "react";
 
-export default function Home() {
+
+function CompA() {
   return (
-    <h1> Hello World</h1>
+    <div>
+      <h1> CompA</h1>
+      <p> This is a comp A </p>
+      <CompB />
+    </div>
   )
 }
+
+function CompB() {
+  return (
+    <div>
+      <h1>CompB</h1>
+      <p>This is comp B </p>
+    </div>
+  )
+}
+
+class CompC extends React.Component {
+  render() {
+    return (
+      <h1>CompC</h1>
+    )
+  }
+}
+
+
+function Home() {
+  return (
+    <div>
+      <h1> Hello World</h1>
+      <CompA />
+      <CompC />
+    </div>
+  )
+}
+
+
+
+
+export default Home;
